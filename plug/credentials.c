94630aab41044ec3bda00e0e04594fda72b1454d
Daniel Gretzke <daniel@gretzke.de>
2019-02-03 22:30:37 +0100

    updated credentials description

--- plug/credentials.c
+++ plug/credentials.c
@@ -1,8 +1,9 @@
-#include "credentials.h";
+#include "src/credentials.h";
 
 struct Credentials credentials = {
-  "Ethereum private key without 0x",
-  "Ethereum address of private key with 0x",
-  "Your Wifi SSID",
-  "Your Wifi password"
-};
+    "Ethereum private key without 0x prefix and lowercase",
+    "Ethereum address of private key with 0x prefix and lowercase",
+    "Your Wifi SSID",
+    "Your Wifi password",
+    "http(s)://your-ethereum-node-address/",
+    "Address of smart contract with 0x prefix and lowercase"};
