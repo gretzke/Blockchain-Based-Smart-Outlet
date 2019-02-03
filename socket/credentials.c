5fdecdbd22e9668a7fb28b89f4b8a7b3eb5cc79a
Daniel Gretzke <daniel@gretzke.de>
2019-02-03 22:30:53 +0100

    added credentials file

---
+++ socket/credentials.c
@@ -0,0 +1,9 @@
+#include "src/credentials.h";
+
+struct Credentials credentials = {
+    "Ethereum private key without 0x prefix and lowercase",
+    "Ethereum address of private key with 0x prefix and lowercase",
+    "Your Wifi SSID",
+    "Your Wifi password",
+    "http(s)://your-ethereum-node-address/",
+    "Address of smart contract with 0x prefix and lowercase"};
